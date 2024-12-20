4e8f8e392655a07844e5904af8a6aa55ab5f828a
blushell <micheal.jones89@gmail.com>
2024-12-20 13:55:58 -0500

    update

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/*╔══════════════╗*/
+/*║  PARAMETERS  ║*/
+/*╚══════════════╝*/
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+let debugMode = false;
+
+/*╔═════════════════════════════════╗*/
+/*║  SRTEAMER.BOT WEBSOCKET SERVER  ║*/
+/*╚═════════════════════════════════╝*/
